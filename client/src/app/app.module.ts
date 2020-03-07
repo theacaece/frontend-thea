85dca38338d79845e9a593bab365bfe4eeacee0e
Maximiliano Astengo <maxiastengo@gmail.com>
2020-03-07 20:39:07 -0300

    avance admistracion de usuarios

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,9 +2,16 @@ import { ErrorInterceptor } from './_helpers/error.interceptor';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolbarModule, MatIconModule, MatProgressSpinnerModule, MatGridListModule } from '@angular/material';
+import {
+  MatButtonModule, MatCardModule, MatInputModule,
+  MatListModule, MatToolbarModule, MatIconModule,
+  MatProgressSpinnerModule, MatGridListModule,
+} from '@angular/material';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
+import {MatTableModule} from '@angular/material/table';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Route } from '@angular/router';
 
@@ -18,10 +25,14 @@ import { UserListComponent } from './users/user-list/user-list.component';
 //import { fakeBackendProvider } from './_helpers/fake-backend';
 import { JwtInterceptor } from './_helpers/jwt.interceptor';
 import { UserEditComponent } from './users/user-edit/user-edit.component';
+import { Error403Component } from './httpError/error403/error403.component';
+import { Error500Component } from './httpError/error500/error500.component';
+import { ExitButtonComponent } from './buttons/exit-button/exit-button.component';
+import { BackButtonComponent } from './buttons/back-button/back-button.component';
 
 const routes: Route[] = [
-  {path: '', component: AppComponent},
-  {path: 'login', component: LoginComponent}
+  { path: '', component: AppComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
@@ -31,7 +42,11 @@ const routes: Route[] = [
     HomeComponent,
     HeaderComponent,
     UserListComponent,
-    UserEditComponent
+    UserEditComponent,
+    Error403Component,
+    Error500Component,
+    ExitButtonComponent,
+    BackButtonComponent,
   ],
   imports: [
     BrowserModule,
@@ -48,7 +63,10 @@ const routes: Route[] = [
     MatProgressSpinnerModule,
     MatGridListModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatTableModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
@@ -56,7 +74,7 @@ const routes: Route[] = [
 
     // provider used to create fake backend
     //fakeBackendProvider
-],
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- client/src/app/users/user-list/user-list.component.ts
+++ client/src/app/users/user-list/user-list.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { UserService } from 'src/app/_services/user.service';
+import { first } from 'rxjs/operators';
+import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+
+export interface UserData {
+  id: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  email: string;
+  password: string;
+  edit: any;
+}
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
+
 export class UserListComponent implements OnInit {
+  loading = false;
+  users: any;
+  displayedColumns: string[] = ['id', 'firstName', 'lastName', 'username','email', 'edit'];
+  //displayedColumns: string[] = ['lastName', 'lastName', 'lastName', 'lastName','lastName'];
+  dataSource: MatTableDataSource<UserData>;
 
-  constructor() { }
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
 
-  ngOnInit() {
+  constructor(private userService: UserService) {
+    // Assign the data to the data source for the table to render
+    
   }
 
+  ngOnInit() {
+    this.loading = true;
+    this.userService.getAll().pipe(first()).subscribe(users => {
+      this.loading = false;
+      this.users = users;
+      this.dataSource = new MatTableDataSource(this.users);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
