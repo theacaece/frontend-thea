14d50ca2e96ca7c7da8fe3edb63625cc729cce02
natig0n <marianataliagonzalez.ar@gmail.com>
2020-03-07 20:53:10 -0300

    Limpieza comentarios

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -97,9 +97,6 @@ const routes: Route[] = [
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-
-    // provider used to create fake backend
-    //fakeBackendProvider
 ],
   bootstrap: [AppComponent]
 })
