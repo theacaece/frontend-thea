58e5b9876c04706bd1da315a3bc70e007c64e4c8
matiasbavauala <matias.bava@uala.com.ar>
2020-03-23 21:43:58 -0300

    Add entrenamiento service

--- client/src/app/configuration/configuration/configuration.component.ts
+++ client/src/app/configuration/configuration/configuration.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { first } from 'rxjs/operators';
 
+import { EntrenamientoService } from '../../_services/entrenamiento.service';
+
 export interface Tile {
   color: string;
   cols: number;
@@ -17,11 +19,34 @@ export interface Tile {
 export class ConfigurationComponent implements OnInit {
 
   loading = false;
-  users: any;
+  resultado: boolean = false;
+  enviado: boolean = true;
+  error = '';
+  
 
-  constructor() { }
+  constructor(private entrenamientoService: EntrenamientoService) { 
+    
+  }
 
   ngOnInit() {
 
   }
+
+  onClick() {
+
+    this.loading = true;
+    this.entrenamientoService;
+
+    this.entrenamientoService.post().subscribe(
+      data => {
+        this.loading = false;
+        this.resultado = true;
+        this.enviado = true;
+      },
+      error => {
+        this.error = error;
+        this.loading = false;
+      });
+  }
+
 }
